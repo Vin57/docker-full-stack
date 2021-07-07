778f1dcc0d55ab8ae0ea96a0c907ae2fe3df8c3d
Vincent <vincent.philippe18@gmail.com>
2021-07-07 17:36:30 +0200

    Prod conf

--- api/src/index.js
+++ api/src/index.js
@@ -1,12 +1,14 @@
 const server = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const MonUrl = process.env.NODE_ENV === 'production' ? `mongodb://${process.env.MONGO_APP_USER}:${process.env.MONGO_APP_PWD}@db` : `mongodb://db`
+let clientDb;
 
 MongoClient.connect(`${MonUrl}`, {useUnifiedTopology: true}, (err, client) => {
     if(err) {
         console.log(err);
     } else {
         console.log('connection db ok !');
+        clientDb = client;
         count = client.db('test').collection("count");
     }
 });
@@ -21,6 +23,18 @@ serv.get('/api/count', (req, res) => {
     }
 )
 
+const app = serv.listen(80); 
 
-
-serv.listen(80); 
+process.addListener('SIGINT', () => {
+    app.close((err) => {
+        if (err) {
+            process.exit(1);
+        } else {
+            if (clientDb) {
+                clientDb.close((err) => process.exit(err ? 1 : 0));
+            } else {
+                process.exit(0);
+            }            
+        }
+    });
+});
